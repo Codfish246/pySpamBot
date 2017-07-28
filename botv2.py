abe84e9048f99bf0964bdfcfc676acd1c31f314e
Luca <Codfish246@users.noreply.github.com>
2017-07-28 11:33:22 +0100

    New version/V2, reworked

---
+++ botv2.py
@@ -0,0 +1,43 @@
+import discord
+import asyncio
+import random
+import time
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------')
+
+prefix = '!'
+delay = 1
+numMessages = 1000 #number of messages to spam, 0 for infinite
+
+sentCount = 0
+
+@client.event
+async def on_message(message):
+    global count
+    global prefix
+    global numMessgaes
+    global sentCount
+    if message.content.startswith('!numberspam'):
+        if numMessages > 0:
+        	for i in range(0, numMessages):
+        		await client.send_message(message.channel, random.randint(1,1000))
+        		sentCount += 1
+        		print("sent", sentCount)
+        		time.sleep(delay)
+        elif numMessgaes == 0:
+        	while(True):
+        		await client.send_message(message.channel, random.randint(1,1000))
+        		sentCount += 1
+        		print("sent", sentCount)
+        		time.sleep(delay)
+sentCount = 0
+    #if message.content.startswith("logout"): #test logout cmd
+    	#self.logout()
+client.run('token', bot=False)
